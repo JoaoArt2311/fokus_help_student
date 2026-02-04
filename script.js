b572dca17be3e519adcd25a879ed7f11ceba383d
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2026-02-04 12:50:15 -0300

    Adicionei um comentario

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Ajuste do botão (mudança de cenario)*/
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
