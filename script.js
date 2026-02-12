128ea60e43c3e09ffe2cf51454fbbf78840ba9fe
João Athur M Nunes <joaoarthurmendesnunes8@gmail.com>
2026-02-11 21:55:23 -0300

    Atualização
    Fizemos a parte de mudar o texto da página

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -16,8 +17,29 @@ curtoBt.addEventListener('click', () => {
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
 })
+
+
+
 /*Função (contexto) */ 
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    
+    /*Mudar titulos*/ 
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+
+        default:
+            break;
+    }
 }
+
